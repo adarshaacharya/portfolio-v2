55d04f831704c1fd3667f3d45796cd60a30add3d
Aadarsha <connectwithadarsha@gmail.com>
2020-05-28 10:09:21 +0545

    :speech_balloon: writing random blog posts for test

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,6 +6,28 @@ import SEO from "../components/seo"
 
 import styles from "./blog-post.scss"
 
+const BlogPostTemplate = ({ data: { mdx: post } }) => (
+  <Layout>
+    <SEO
+      title={post.frontmatter.title}
+      author={post.frontmatter.author}
+      description={post.frontmatter.description}
+    />
+
+    <h1>{post.frontmatter.title}</h1>
+    <p>
+      Posted by {post.frontmatter.author} on {post.frontmatter.date}{" "}
+    </p>
+    <i>{post.fields.readingTime.text}</i>
+    <MDXRenderer>{post.body}</MDXRenderer>
+    <Link to="/blog">&larr; Back to posts</Link>
+  </Layout>
+)
+
+export default BlogPostTemplate
+
+
+
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
@@ -23,19 +45,3 @@ export const query = graphql`
     }
   }
 `
-
-const BlogPostTemplate = ({ data: { mdx: post } }) => (
-  <Layout>
-    <SEO title={post.frontmatter.title} author={post.frontmatter.author} description={post.frontmatter.description}/>
-
-    <h1>{post.frontmatter.title}</h1>
-    <p>
-      Posted by {post.frontmatter.author} on {post.frontmatter.date}{" "}
-    </p>
-    <i>{post.fields.readingTime.text}</i>
-    <MDXRenderer>{post.body}</MDXRenderer>
-    <Link to="/blog">&larr; Back to posts</Link>
-  </Layout>
-)
-
-export default BlogPostTemplate
