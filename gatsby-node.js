aa2b080d587c9d1d2df6a91b1a933424caade695
Aadarsha <connectwithadarsha@gmail.com>
2020-05-28 14:59:29 +0545

    :recycle: next and previous post added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,15 @@
+const path = require(`path`)
+
 exports.createPages = async ({ actions, graphql, resporter }) => {
+  const blogPost = path.resolve("./src/templates/blog-post.js")
+
   const result = await graphql(`
     query {
-      allMdx {
+      allMdx(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
         nodes {
           frontmatter {
             slug
+            title
           }
         }
       }
@@ -15,14 +20,20 @@ exports.createPages = async ({ actions, graphql, resporter }) => {
     reporter.panic("failed to create posts", result.errors)
   }
 
+  // all post pages
   const posts = result.data.allMdx.nodes
 
-  posts.forEach(post => {
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1]
+    const next = index === 0 ? null : posts[index - 1]
+
     actions.createPage({
-      path: post.frontmatter.slug, // url of each post
-      component: require.resolve("./src/templates/blog-post.js"),
+      path: post.frontmatter.slug, // url of each post will bve slug
+      component: blogPost,
       context: {
         slug: post.frontmatter.slug,
+        previous,
+        next,
       },
     })
   })
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,30 +4,55 @@ import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import styles from "./blog-post.scss"
+import styles from "./style.module.scss"
 
-const BlogPostTemplate = ({ data: { mdx: post } }) => (
-  <Layout>
-    <SEO
-      title={post.frontmatter.title}
-      author={post.frontmatter.author}
-      description={post.frontmatter.description}
-    />
+const BlogPostTemplate = ({ data, pageContext }) => {
+  const post = data.mdx
+  const { previous, next } = pageContext
+  return (
+    <Layout>
+      <SEO
+        title={post.frontmatter.title}
+        author={post.frontmatter.author}
+        description={post.frontmatter.description}
+      />
 
-    <h1>{post.frontmatter.title}</h1>
-    <p>
-      Posted by {post.frontmatter.author} on {post.frontmatter.date}{" "}
-    </p>
-    <i>{post.fields.readingTime.text}</i>
-    <MDXRenderer>{post.body}</MDXRenderer>
-    <Link to="/blog">&larr; Back to posts</Link>
-  </Layout>
-)
+      <div className={styles.currentBlogPost}>
+          <h1>{post.frontmatter.title}</h1>
+          <p>
+            Posted by {post.frontmatter.author} on {post.frontmatter.date}{" "}
+          </p>
+          <i>{post.fields.readingTime.text}</i>
+          <MDXRenderer>{post.body}</MDXRenderer>
+       
+        <hr />
+      </div>
+
+      <nav className={styles.bottomNav}>
+        <ul>
+          <li>
+            {previous && (
+              <Link to={previous.frontmatter.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.frontmatter.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  )
+}
 
 export default BlogPostTemplate
 
-
-
+// query to retureve current blog info
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
