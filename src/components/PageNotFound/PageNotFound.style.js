cd2762f18297435ec18d5462603aa6efc648ae80
Aadarsha <connectwithadarsha@gmail.com>
2020-07-07 16:32:22 +0545

    :recycle:  REFACTOR: Layout and PageNotfound component styled-components

---
+++ src/components/PageNotFound/PageNotFound.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledPageNotFound = styled.div`
+  height: 65vh;
+
+  h1 {
+    font-family: Montserrat, sans-serif;
+    font-weight: 900 !important;
+    font-size: 35px;
+  }
+`
