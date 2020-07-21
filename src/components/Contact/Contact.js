c159b1aa0d97236cb5531a7ed65decb33ae73e30
Aadarsha <connectwithadarsha@gmail.com>
2020-07-22 02:09:54 +0545

    :heavy_minus_sign: REMOVE : Google recaptcha deleted

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,6 @@
 import useForm from '@hooks/use-form';
 import { SectionTitle } from '@portfolio-ui/';
 import React from 'react';
-import Recaptcha from 'react-google-recaptcha';
 import {
   ContactButton,
   ContactForm,
@@ -15,12 +14,6 @@ import {
 
 const Contact = () => {
   const [formData, handleInput] = useForm();
-  const recaptchaRef = React.createRef();
-
-  const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY;
-  if (typeof RECAPTCHA_KEY === 'undefined') {
-    throw new Error('RECAPTCHA KEY is undefined');
-  }
 
   return (
     <>
@@ -36,15 +29,22 @@ const Contact = () => {
         </FormDescription>
 
         <Form
-          name="contact-adarsha"
+          name="contact"
+          method="post"
           data-netlify="true"
           data-netlify-recaptcha="true"
           data-netlify-honeypot="bot-field"
           autoComplete="off"
         >
-          <noscript>
-            <p>This form won’t work with Javascript disabled</p>
-          </noscript>
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{' '}
+              <input name="bot-field" onChange={handleInput} />
+            </label>
+          </p>
+
           <FormContainer>
             <FormGroup style={{ gridArea: 'name' }}>
               <label htmlFor="name">Name*</label>
@@ -87,11 +87,6 @@ const Contact = () => {
             </FormGroup>
           </FormContainer>
 
-          <Recaptcha
-            ref={recaptchaRef}
-            sitekey={RECAPTCHA_KEY}
-            size="compact"
-          />
           <ContactButton type="submit">Submit</ContactButton>
         </Form>
       </ContactForm>
