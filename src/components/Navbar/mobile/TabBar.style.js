90f80bb8f38c3d86bb6eb17f058016bdd5ab1767
Aadarsha <connectwithadarsha@gmail.com>
2020-08-01 16:06:50 +0545

    :lipstick: style(tabbar): Tabbar bottom padding fixed
    #

--- src/components/Navbar/mobile/TabBar.style.js
+++ src/components/Navbar/mobile/TabBar.style.js
@@ -15,7 +15,7 @@ export const TabItems = styled.div`
 `;
 
 export const TabItem = styled(Link)`
-  padding: 1rem 0 1.5rem 0;
+  padding: 1rem 0 0rem 0;
   color: ${props => props.theme.primaryBlack};
   flex: 1;
   display: flex;
