db11d9116f9a5f2f0a9de00dc6c15d9743cb0714
Aadarsha <connectwithadarsha@gmail.com>
2020-08-25 00:26:23 +0545

    feat: env variable setup for gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,9 @@
 const config = require('./config/SiteConfig');
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: config.siteTitle,
