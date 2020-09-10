6aeb1df5dd25484b0c5df8e4c58f4fa3cb4d37c3
Aadarsha <connectwithadarsha@gmail.com>
2020-09-10 08:35:47 +0545

    style: newsletter section styled

--- src/components/Subscribe/Subscribe.js
+++ src/components/Subscribe/Subscribe.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { tinyLetter } from '../../../config/SiteConfig';
-import { SubscribeContainer, SubscribeForm, SubscribeTitle } from './Subscribe.style';
+import {
+    SubmitBtn,
+    SubscribeContainer,
+    SubscribeForm,
+    SubscribeTitle,
+    Input
+} from './Subscribe.style';
 const Subscribe = () => {
   return (
     <>
       <SubscribeContainer>
-          <SubscribeTitle>News Letter</SubscribeTitle>
+        <SubscribeTitle>News Letter</SubscribeTitle>
         <p>
-          Subscribe to get notified with awesome articles deliverd directly in your inbox. I don't spam, really!{' '}
+          Subscribe to get notified with awesome articles deliverd directly in
+          your inbox. I don't spam, really!{' '}
         </p>
         <SubscribeForm
           action={`https://tinyletter.com/${tinyLetter}`}
@@ -15,7 +22,7 @@ const Subscribe = () => {
           target="popupwindow"
           onSubmit={`window.open('https://tinyletter.com/${tinyLetter}', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true`}
         >
-          <input
+          <Input
             type="email"
             name="email"
             id="email"
@@ -23,7 +30,7 @@ const Subscribe = () => {
             required
           />
           <input type="hidden" value="1" name="embed" />
-          <input type="submit" value="Subscribe" />
+          <SubmitBtn type="submit">Subscribe</SubmitBtn>
         </SubscribeForm>
       </SubscribeContainer>
     </>
--- src/components/Subscribe/Subscribe.style.js
+++ src/components/Subscribe/Subscribe.style.js
@@ -1,13 +1,14 @@
 import { SectionTitle } from '@portfolio-ui/';
+import { Button } from '@src/@portfolio-ui';
 import styled from 'styled-components';
+import { FormControl } from '../Contact/Contact.style';
 
 export const SubscribeContainer = styled.div`
-  padding: 2rem;
+  padding: 1rem;
   margin-bottom: 5rem;
   border-radius: 20px;
-
   background: ${props => props.theme.cardBg};
-  border-top: 4px solid #73c4fa;
+  border-top: 5px solid #73c4fa;
   box-shadow: ${props =>
     props.theme.dark
       ? '0px 5px 0px rgba(0, 151, 251, 0.25)'
@@ -15,16 +16,28 @@ export const SubscribeContainer = styled.div`
 
   p {
     font-size: 1rem;
+    margin-top: 15px;
   }
 `;
 
 export const SubscribeTitle = styled(SectionTitle)`
   margin: 0;
-  padding: 10px 0;
+  padding: 0;
   text-align: center;
 `;
 
 export const SubscribeForm = styled.form`
   display: flex;
-  margin-top: 2rem;
+  align-items: center;
+  gap: 20px;
+  max-width: 600px;
+  margin: 2rem auto;
+`;
+
+export const SubmitBtn = styled(Button)`
+  padding: 0.8rem 1.5rem;
+`;
+
+export const Input = styled.input`
+  ${FormControl}
 `;
