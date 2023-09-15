1fb35c56420106a3f28e75c86843783b19a59b16
Aadarsha Acharya <connectwithadarsha@gmail.com>
2023-09-16 01:23:13 +0545

    Update AuthorInfo.js

--- src/components/AuthorInfo/AuthorInfo.js
+++ src/components/AuthorInfo/AuthorInfo.js
@@ -42,15 +42,12 @@ const AuthorInfo = () => {
 
       <Bio>
         Hello! I’m Aadarsha Acharya, fullstack JavaScript developer based in
-        Kathmandu Nepal, currently working at&nbsp;
-        <ExternalLink href="https://renegadeinsurance.com/">
-          Renegade Insurance
-        </ExternalLink>
-        &nbsp;as Software Development Engineer-I.
+        Kathmandu Nepal, working at&nbsp;
+        &nbsp;as Software Engineer at game-tech domain.
       </Bio>
       <Bio>
         I'm super passionate about exploring and building open source web apps
-        and tools. Currently I'm diving in the React ecosystem with backend in
+        and tools. Currently I'm working in the React ecosystem with backend in
         Node.js.
       </Bio>
 
